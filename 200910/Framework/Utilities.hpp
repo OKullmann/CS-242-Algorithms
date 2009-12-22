9281e7acff33988a910549004ad02ac9d56eb6d2
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-12-22 18:41:04 +0000

    Tool for output of containers.
    And some further documentation.

--- 200910/Framework/Utilities.hpp
+++ 200910/Framework/Utilities.hpp
@@ -11,12 +11,20 @@
 #include <string>
 #include <sstream>
 #include <stdexcept>
+#include <ostream>
 
 namespace IntroAlgo {
   namespace Framework {
 
+    /*!
+      \enum ExperimentErrors
+      \brief Error return-codes for experiment-programs
+    */
     enum ExperimentErrors { parameter_error = 1 };
 
+    /*!
+      \brief Converting string into type T
+    */
     template <typename T>
     T lexical_cast(const std::string& x) {
       std::stringstream s;
@@ -28,6 +36,30 @@ namespace IntroAlgo {
       return result;
     }
 
+    /*!
+      \class Wrapper
+      \brief Wrapping containers, so that the stream-inserter can be overloaded.
+    */
+    template <class Container>
+    struct Wrapper {
+      const Container& s;
+      Wrapper(const Container& s) : s(s) {}
+    };
+    /*!
+      \brief Output of containers
+
+      Use "out << cw(X)" for output of container X.
+    */
+    template <class Container>
+    inline Wrapper<Container> cw(const Container& s) { return Wrapper<Container>(s); }
+    template <class Container>
+    std::ostream& operator <<(std::ostream& out, const Wrapper<Container>& s) {
+      typedef typename Container::const_iterator iterator;
+      const iterator end = s.s.end();
+      for (iterator i = s.s.begin(); i != end; ++i)
+        out << *i << " ";
+      return out;
+    }
 
   }
 }
