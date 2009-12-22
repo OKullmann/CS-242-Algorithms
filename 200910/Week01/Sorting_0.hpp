12fcc8babb89c368747c5df05cececdc4680ba99
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-12-22 16:03:16 +0000

    Completed insertion-sort.

--- 200910/Week01/Sorting_0.hpp
+++ 200910/Week01/Sorting_0.hpp
@@ -16,8 +16,22 @@ namespace IntroAlgo {
   namespace Week01 {
 
     template <typename value_type>
-    void selection_sort(std::vector<value_type> V) {
+    void selection_sort(std::vector<value_type>& V) {
+      
+    }
 
+    template <typename value_type>
+    void insertion_sort(std::vector<value_type>& A) {
+      typedef typename std::vector<value_type>::size_type size_type;
+      for (size_type j = 1; j < A.size(); ++j) {
+        const value_type key = A[j];
+        size_type i = j;
+        while (i > 0 and A[i-1] > key) {
+          A[i] = A[i-1];
+          i = i-1;
+        }
+        A[i] = key;
+      }
     }
 
   }
