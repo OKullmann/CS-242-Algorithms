18fc210333342892c1c7b79ee4ab4802956d21b5
Arnold Beckmann <a.beckmann@swansea.ac.uk>
2010-01-17 11:42:28 +0000

    adjusted Test

--- 200910/Week01/Test.cpp
+++ 200910/Week01/Test.cpp
@@ -10,46 +10,31 @@
 #include <iostream>
 #include <vector>
 
+#include <200910/Framework/Utilities.hpp>
+
 #include <200910/Week01/Sorting.hpp>
 #include <200910/Week01/Generators.hpp>
 
 using namespace std;
 using IntroAlgo::Week01::ArrayGenerators;
 
-namespace {
-
-  void druck(vector<int>& A) {
-    
-    unsigned int i;
-    cout << A[0];
-    for (i = 1; i < A.size(); ++i)
-      cout << ", " << A[i];
-
-    return;
-
-  }
-
-}
-
 
 int main(const int argc, const char* const argv[]) {
 
-  int Alist[] = {9,6,5,7,2,4,1,3,6,8};
+  using namespace IntroAlgo::Framework;
+
+  // int Alist[] = {9,6,5,7,2,4,1,3,6,8};
   vector<int> A;
   /*
   A.assign (Alist, Alist + 10 );
   */
   A = ArrayGenerators::random(10);
 
-  cout << "\nPermutation Sort\n\nInput:   >";
-  druck(A);
-  cout << "<\n\n";
+  cout << "\nInsertion Sort\n\nInput:   >" << cw(A) << "<\n\n";
 
-  IntroAlgo::Week01::permutation_sort(A);
+  IntroAlgo::Week01::insertion_sort(A);
 
-  cout << "\nOutput:  >";
-  druck(A);
-  cout << "<\n\n";
+  cout << "\nOutput:  >" << cw(A) << "<\n\n";
 
   return 0;
 }
