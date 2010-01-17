0f12e5d3dcd4b24b511a6a12879aa739884ceea2
Arnold Beckmann <a.beckmann@swansea.ac.uk>
2010-01-17 11:49:03 +0000

    generate Expertiment_down.cpp from Experiment.cpp

---
+++ 200910/Week01/Experiment_down.cpp
@@ -0,0 +1,44 @@
+// Arnold Beckmann, 17.12.2009 (Swansea)
+
+/*!
+  \file 200910/Week01/Experiment_down.cpp
+  \brief Experiments with sorting algorithms on reverse sorted inputs
+
+  "Experiment_down N" calls the sorting algorithm N times with downwards
+  sorted inputs; the statistics are output to standard output.
+*/
+
+#include <string>
+#include <iostream>
+
+#include <200910/Framework/Measurement.hpp>
+#include <200910/Framework/Utilities.hpp>
+
+#include <200910/Week01/Sorting.hpp>
+#include <200910/Week01/Generators.hpp>
+
+namespace {
+
+  const std::string program = "Experiment";
+  const std::string err = "ERROR[" + program + "]: ";
+
+}
+
+int main(const int argc, const char* const argv[]) {
+  using namespace IntroAlgo::Framework;
+  if (argc != 2) {
+    std::cerr << err << "Exactly one parameter is required, the number N of experiments.\n";
+    return parameter_error;
+  }
+
+  const int N = lexical_cast<int>(argv[1]);
+  OutputMeasurement::header_line("\n\n#  Reverse sorted lists\n size comparisons\n");
+  for (int i = 0; i <= N; ++i) {
+    using IntroAlgo::Week01::ArrayGenerators;
+    typedef ArrayGenerators::vector_type vector_type;
+    vector_type A = ArrayGenerators::sorted_r(i);
+    IntroAlgo::Week01::insertion_sort(A);
+    // std::cout << cw(A) << "\n";
+  }
+  
+}
