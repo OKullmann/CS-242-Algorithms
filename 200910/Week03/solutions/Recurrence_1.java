93622fb0dd5a483ccbee7a7f7237608813d65c6c
Arnold Beckmann <a.beckmann@swansea.ac.uk>
2010-02-08 13:26:03 +0000

    further tries for week 3

--- 200910/Week03/solutions/Recurrence_1.java
+++ 200910/Week03/solutions/Recurrence_1.java
@@ -12,22 +12,24 @@ class Recurrence {
     static long counter;
 
     public static void recurrence(int n) {
+//	ExecutionTimer.start();
 	counter = 0;
 	T(n);
+//	ExecutionTimer.end();
+//	System.out.println(" " + n + " " + ExecutionTimer.duration() );
 	System.out.println(" " + n + " " + counter );
     }
 
     static void T(int n) {
-	if (n<2)
+	if (n<4)
 	    sleep();
 	else {
 	    int i = (int) Math.floor(n/2.0);
 	    int j = (int) Math.ceil(n/2.0);
-//	System.out.println("n,i,j:  " + n + " " + i + " " + j );
-	    T(i);
+	    T(i-1);
 	    T(i);
 	    T(j);
-	    T(j);
+	    T(j+1);
 	    for (j = 1; j < n*n*n; j++) 
 		sleep();
 	}
@@ -54,7 +56,9 @@ class Recurrence2 {
 	    int j = (int) Math.ceil(n/2.0);
 //	    return (4*T(j)+n);
 //	    return (2*T(i)+2*T(j)+n);
-	    return (T(i-1)+T(i)+T(j)+T(j+1)+n);
+//	    return (T(i-2)+T(i-1)+T(i)+T(i+1)+n);
+	    return (T(i-1)+2*T(i)+T(i+1)+n);
+//	    return (T(i-1)+T(i)+T(j)+T(j+1)+n);
 	}
     }
 
