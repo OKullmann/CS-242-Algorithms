f5e19dd8ba3ddbdf7af2a93ac58952ae9d66771a
Arnold Beckmann <a.beckmann@swansea.ac.uk>
2010-02-08 20:23:16 +0000

    Some cleaning up of superflous files

---
+++ 200910/Week03/Recurrence_0.java
@@ -0,0 +1,23 @@
+// Arnold Beckmann, 05.02.2009 (Swansea)
+
+
+/*
+  File:  200910/Week03/solutions/Recurrence_1.java
+  Descr: Experience the Master Theorem
+*/
+
+
+class Recurrence {
+
+    public static void recurrence(int n) {
+	System.out.println(" " + n + " " + ((long) T((double) n)) );
+    }
+
+    static double T(double n) {
+	if (n<2)
+	    return 1;
+	else
+	    return (4*T(n/2)+n);
+    }
+
+}
