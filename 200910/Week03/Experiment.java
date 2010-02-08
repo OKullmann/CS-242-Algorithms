f30c1004ce2c20acb3cb34e07f654cacc6bc2fd3
Arnold Beckmann <a.beckmann@swansea.ac.uk>
2010-02-08 20:21:40 +0000

    More changes to week 3

--- 200910/Week03/Experiment.java
+++ 200910/Week03/Experiment.java
@@ -28,7 +28,7 @@ class Experiment {
 
 	System.out.println("  size executionTime\n");
 	for (int i = 0; i <= N; i++) {
-	    Recurrence2.recurrence(i);
+	    Recurrence.recurrence(i);
 	}
 
     }
--- 200910/Week03/solutions/Recurrence_1.java
+++ 200910/Week03/solutions/Recurrence_1.java
@@ -9,60 +9,15 @@
 
 class Recurrence {
 
-    static long counter;
-
     public static void recurrence(int n) {
-//	ExecutionTimer.start();
-	counter = 0;
-	T(n);
-//	ExecutionTimer.end();
-//	System.out.println(" " + n + " " + ExecutionTimer.duration() );
-	System.out.println(" " + n + " " + counter );
+	System.out.println(" " + n + " " + ((long) T((double) n)) );
     }
 
-    static void T(int n) {
-	if (n<4)
-	    sleep();
-	else {
-	    int i = (int) Math.floor(n/2.0);
-	    int j = (int) Math.ceil(n/2.0);
-	    T(i-1);
-	    T(i);
-	    T(j);
-	    T(j+1);
-	    for (j = 1; j < n; j++) 
-		sleep();
-	}
-    }
-
-    static void sleep() {
-	counter++;
-    }
-
-}
-
-
-class Recurrence2 {
-
-    static long[] M = { 1, 1, 6, 17, 28 };
-
-    public static void recurrence(int n) {
-	System.out.println(" " + n + " " + T(n) );
-    }
-
-    static long T(int n) {
-	if (n<4)
-	    return M[n];
-	else {
-	    int i = (int) Math.floor(n/2.0);
-	    int j = (int) Math.ceil(n/2.0);
-//	    return (4*T(i)+n);
-//	    return (2*T(i)+2*T(j)+n);
-//	    return (T(i-2)+T(i-1)+T(i)+T(i+1)+n);
-//	    return (2*(T(i-1)+T(i+1))+n);
-//	    return (T(i-1)+2*T(i)+T(i+1)+n);
-	    return (T(i-1)+T(i)+T(j)+T(j+1)+n);
-	}
+    static double T(double n) {
+	if (n<2)
+	    return 1;
+	else
+	    return (4*T(n/2)+n);
     }
 
 }
