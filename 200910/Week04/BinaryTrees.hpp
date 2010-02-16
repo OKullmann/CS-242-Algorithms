49f70749fd2647f08d9d6f00f628f5a741be8181
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-02-16 04:04:26 +0000

    Added code for insertion.
    And mode Node a private member of Tree.

--- 200910/Week04/BinaryTrees.hpp
+++ 200910/Week04/BinaryTrees.hpp
@@ -12,35 +12,32 @@ namespace BinaryTrees {
   typedef int key_type;
   typedef int satellite_type;
 
-  struct Node {
-    const Node* p;
-    const Node* left;
-    const Node* right;
-    key_type key;
-    satellite_type s;
-  };
-
   class Tree {
+
+    struct Node {
+      const Node* p;
+      const Node* left;
+      const Node* right;
+      key_type key;
+      satellite_type s;
+    };
+
   public :
 
     typedef long int int_type;
-    typedef Node node_type;
+    typedef const Node* public_node_type;
 
     Tree() : root(0) {}
 
-    int_type height() const {
-      return height(root);
-    }
-    int_type height(const node_type* const x) const {
+    int_type height() const { return height(root); }
+    int_type height(const public_node_type x) const {
       if (x == 0) return -1;
       return std::max(height(x->left), height(x->right)) + 1;
     }
 
     // walk the tree in inorder, and output "(key,s)" to "out"
-    void inorder_walk(std::ostream& out) const {
-      inorder_walk(out, root);
-    }
-    void inorder_walk(std::ostream& out, const node_type* const x) const {
+    void inorder_walk(std::ostream& out) const { inorder_walk(out, root); }
+    void inorder_walk(std::ostream& out, const public_node_type x) const {
       if (x == 0) return;
       inorder_walk(out, x->left);
       out << "(" << x->key << ", " << x->s << ")"; // print key and s
@@ -48,55 +45,70 @@ namespace BinaryTrees {
     }
 
     // return NULL if k is not present in the tree
-    const node_type* search(const key_type k) const {
-      return search(root, k);
-    }
-    const node_type* search(const node_type* const x, const key_type k) const {
+    public_node_type search(const key_type k) const { return search(root, k); }
+    public_node_type search(const public_node_type x, const key_type k) const {
       if (x == 0 or x->key == k) return x;
       if (k <= x->key) return search(x->left, k);
       return search(x->right, k);
     }
 
     // returns NULL iff tree is empty
-    const node_type* minimum() const {
-      return minimum(root);
-    }
-    const node_type* minimum(const node_type* x) const {
+    public_node_type minimum() const { return minimum(root); }
+    public_node_type minimum(public_node_type x) const {
       if (x == 0) return 0;
       while (x->left != 0) x = x->left;
       return x;
     }
 
     // returns NULL iff tree is empty
-    const node_type* maximum() const {
-      return maximum(root);
-    }
-    const node_type* maximum(const node_type* x) const {
+    public_node_type maximum() const { return maximum(root); }
+    public_node_type maximum(public_node_type x) const {
       if (x == 0) return 0;
       while (x->right != 0) x = x->right;
       return x;
     }
 
     // return NULL iff x is the last node
-    const node_type* successor(const node_type* x) {
+    public_node_type successor(public_node_type x) {
       assert(x != 0);
       if (x->right != 0) return minimum(x->right);
-      const node_type* y = x->p;
+      public_node_type y = x->p;
       while (y != 0 and x == y->right) { x = y; y = x->p; }
       return y;
     }
 
     // return NULL iff x is the first node
-    const node_type* predecessor(const node_type* x) {
+    public_node_type predecessor(public_node_type x) {
       assert(x != 0);
       if (x->left != 0) return maximum(x->left);
-      const node_type* y = x->p;
+      public_node_type y = x->p;
       while (y != 0 and x == y->left) { x = y; y = x->p; }
       return y;
     }
 
+    Tree& insert(const key_type k, const satellite_type s) {
+      Node* const z = new Node;
+      z->p = 0; z->left = 0; z->right = 0;
+      z->key = k; z->s = s;
+      if (root == 0) { root = z; return *this; }
+      Node* y = 0;
+      public_node_type x = root;
+      while (x != 0) {
+        y = const_cast<Node*>(x);
+        if (x->key <= z->key) x = x->right;
+        else x = x->left;
+      }
+      z->p = y;
+      if (y->key <= z->key) y->right = z;
+      else y->left = z;
+      return *this;
+    }
+
+    /* Remark: Since insert returns a reference to Tree, we can chain inserts,
+    for example "T.insert(10,1).insert(-11,2)". */
+
   private :
-    const node_type* root;
+    public_node_type root;
   };
 
 }
