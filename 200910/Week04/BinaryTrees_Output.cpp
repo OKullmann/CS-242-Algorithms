844ccf6bfe4073b82a64b31c267f5c7e4215cf1e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-02-16 22:52:20 +0000

    Allowing also to search for keys.

--- 200910/Week04/BinaryTrees_Output.cpp
+++ 200910/Week04/BinaryTrees_Output.cpp
@@ -2,13 +2,16 @@
 
 /*!
   \file BinaryTree.cpp
-  \brief Data from standard input is put into a binary search tree, and data about the tree is output
+  \brief Reading data into a binary search tree, printing data on the tree, and then waiting for queries
 
-  The data is just a stream of integers. The "satellite data" is a counter,
-  starting with 1.
+  Data from standard input is put into a binary search tree, and data about
+  the tree is output. The data consists of integers, and is augmented with
+  counter-data (as "satellite data", starting with 1). After that a loop is
+  entered, where the tree can be searched for integers.
 */
 
 #include <iostream>
+#include <string>
 
 #include "BinaryTrees.hpp"
 
@@ -25,4 +28,18 @@ int main() {
   std::cout << "Inorder walk:\n"; T.inorder_walk(std::cout); std::cout << "\n";
   std::cout << "Minimum = " << T.minimum() << "\n";
   std::cout << "Maximum = " << T.maximum() << "\n";
+
+  std::cin.clear();
+  std::cout << "\nSearching for keys:\n";
+  for (;;) {
+    BinaryTrees::key_type k;
+    if (not (std::cin >> k)) break;
+    const BinaryTrees::Tree::public_node_type p = T.search(k);
+    std::cout << "search for " << k << " yields " << p << "\n";
+    if (p) {
+      std::cout << "predecessor of " << p << " is " << T.predecessor(p);
+      std::cout << ", successor is " << T.successor(p) << "\n";
+    }
+  }
+
 }
