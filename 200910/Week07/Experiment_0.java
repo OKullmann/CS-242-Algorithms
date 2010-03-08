e45b82a00333dcb1fd0823b316fc8bca58d6b26e
Arnold Beckmann <a.beckmann@swansea.ac.uk>
2010-03-08 10:45:57 +0000

    distributed files to _0 and _1 in solutions

--- 200910/Week07/Experiment_0.java
+++ 200910/Week07/Experiment_0.java
@@ -1,15 +1,15 @@
 // Arnold Beckmann, 05.03.2010 (Swansea)
 
 /*
-  File:  200910/Week07/Experiment.java
+  File:  200910/Week07/Experiment_0.java
   Descr: Experiments with Huffman codes
 */
 
-/*
-  "Experiment_InsertionSort_up N" calls the sorting algorithm N times with upwards
-  sorted inputs; the statistics are output to standard output.
-*/
 
+/*
+  "Experiment" stores the frequencies of letters in the plain text
+  "adeedbede"
+*/
 
 class Experiment {
     public static Node a = new Node('a',1,null,null);
@@ -22,54 +22,12 @@ class Experiment {
 }
 
 
-
-class ExperimentA {
-
-    protected static String program = "Experiment";
-    protected static String err = "ERROR[" + program + "]: ";
-
-
-
-    public static void main(String[] args) {
-
-	//  this program requires one argument on the command line  
-        if (args.length != 1) {
-	    System.err.println(err + "Exactly one parameter is required, the number N of experiments.\n");
-	    return;
-	}
-
-
-	int[][] T = {
-	    {'a',1}, {'b',1}, {'c',0}, {'d',3}, {'e',2}, {'f',4},
-	    {'g',3}, {'h',2}, {'i',6}, {'j',4}, {'k',2}, {'l',1}
-	};
-	
-
-	int n = T.length;
-	Node C[] = new Node[n];
-	for (int i = 0; i < n; i++) {
-	    C[i] = new Node((char)T[i][0],T[i][1],null,null);
-	}
-	Node H = Huffman.generate(C);
-	Huffman.out(C);
-
-	String a = "badffdeeffd";
-	System.out.println("Text:      " + a);
-	String s = Huffman.encode(a);
-	System.out.println("Encoding:  " + s);
-	System.out.println("Decoding:  " + Huffman.decode(H,s) + "\n\n");
-    }
-
-}
-
-
-
-
 /*
-  "ExperimentB" does what???
+  "ExperimentA" implements a non optimal prefix code for text in
+  "Experiment" and applies it to the text.
 */
 
-class ExperimentB extends Experiment {
+class ExperimentA extends Experiment {
 
     protected static String program = "Experiment";
     protected static String err = "ERROR[" + program + "]: ";
@@ -103,44 +61,13 @@ class ExperimentB extends Experiment {
 
 
 
+
 /*
-  "ExperimentC" does what???
+  "ExperimentB" applies Huffman's algorithm to compute an optimal prefix code
+  for text in "Experiment", and applies the computed prefix code to the text.
 */
 
-class ExperimentC extends Experiment {
-
-    protected static String program = "Experiment";
-    protected static String err = "ERROR[" + program + "]: ";
-
-    public static void main(String[] args) {
-
-	//  this program requires no argument on the command line  
-        if (args.length != 0) {
-	    System.err.println(err + "Exactly zero parameters are required.\n");
-	    return;
-	}
-
-	Node H,x,y;
-	x = new Node(7,c,a);
-	x = new Node(7,x,b);
-	y = new Node(8,x,d);
-	H = new Node(9,e,y);
-	Huffman.hc(H);
-
-	Huffman.out(C);
-	String s_plain = "adeedbede";
-	System.out.println("Text:      " + s_plain);
-	String s_code = Huffman.encode(s_plain);
-	System.out.println("Encoding:  " + s_code);
-	System.out.println("Decoding:  " + Huffman.decode(H,s_code) + "\n\n");
-    }
-
-}
-
-
-
-
-class ExperimentD extends Experiment {
+class ExperimentB extends Experiment {
 
     protected static String program = "ExperimentD";
     protected static String err = "ERROR[" + program + "]: ";
@@ -165,9 +92,15 @@ class ExperimentD extends Experiment {
 }
 
 
-class ExperimentE {
 
-    protected static String program = "ExperimentD";
+/*
+  "ExperimentC" applies Huffman's algorithm to compute an optimal prefix code
+  for 8 letters given with some fequencies, and displays the prefix code.
+*/
+
+class ExperimentC {
+
+    protected static String program = "ExperimentC";
     protected static String err = "ERROR[" + program + "]: ";
 
     public static void main(String[] args) {
