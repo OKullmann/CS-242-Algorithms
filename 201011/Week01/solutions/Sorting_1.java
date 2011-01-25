99f36df4fcbfc4d128db27c6f58ad821d9f50093
Arnold Beckmann <a.beckmann@swansea.ac.uk>
2011-01-25 15:57:46 +0000

    Copied Week01 into new 201011 folder

---
+++ 201011/Week01/solutions/Sorting_1.java
@@ -0,0 +1,27 @@
+// Arnold Beckmann, 19.12.2009 (Swansea)
+
+/*
+  File:  200910/Week01/solutions/Sorting_1.hpp
+  Descr: Sorting algorithms
+*/
+
+
+class Insertion_Sort {
+
+    public static void insertion_sort(long[] A) {
+	/// long start = System.nanoTime();
+	for (int j = 1; j < A.length; j++) {
+	    long key = A[j];
+	    int i = j;
+	    while ((i > 0) && (A[i-1] > key)) {
+		A[i] = A[i-1];
+		i = i-1;
+	    }
+	    A[i] = key;
+	}
+ 	/// System.out.println(" " + A.length + " " + (System.nanoTime()-start) );
+
+    }
+
+}
+
