36561eda68216f29b68b0c1c8e591da371548c83
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-02-15 17:08:16 +0000

    Copied programs for week 03.

---
+++ 201011/Week03/solutions/Recurrence_2.java
@@ -0,0 +1,24 @@
+// Arnold Beckmann, 09.02.2009 (Swansea)
+
+
+/*
+  File:  200910/Week03/solutions/Recurrence_2.java
+  Descr: Experience the Master Theorem
+         Computing recurrence T_3
+*/
+
+
+class Recurrence {
+
+    public static void recurrence(int n) {
+	System.out.println(" " + n + " " + ((long) T((double) n)) );
+    }
+
+    static double T(double n) {
+	if (n<2)
+	    return 1;
+	else
+	    return (4*T(n/2)+n*n*n);
+    }
+
+}
