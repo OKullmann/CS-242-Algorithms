e89ce542327efe93770cf40b627500d5ef9535b8
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-09-30 12:14:07 +0100

    Update of indentation, and removal of tabs.

--- 201112/Week01/Sorting.java
+++ 201112/Week01/Sorting.java
@@ -6,20 +6,19 @@
   Descr: Sorting algorithms
 */
 
-
 class Insertion_Sort {
 
-    public static void insertion_sort(final long[] A) {
-	final long start = System.nanoTime();
-	for (int j = 1; j < A.length; ++j) {
-	    final long key = A[j];
-	    int i = j;
-	    while (i > 0 && A[i-1] > key) {
-		A[i] = A[i-1];
-		i = i-1;
-	    }
-	    A[i] = key;
-	}
- 	System.out.println(" " + A.length + " " + (System.nanoTime()-start));
+  public static void insertion_sort(final long[] A) {
+    final long start = System.nanoTime();
+    for (int j = 1; j < A.length; ++j) {
+      final long key = A[j];
+      int i = j;
+      while (i > 0 && A[i-1] > key) {
+        A[i] = A[i-1];
+        i = i-1;
+      }
+      A[i] = key;
     }
+    System.out.println(" " + A.length + " " + (System.nanoTime()-start));
+  }
 }
