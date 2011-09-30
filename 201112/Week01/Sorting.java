238eda67e8f0c14c71325c6f2099245cbe786f57
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-09-30 12:08:17 +0100

    Style improvements.

--- 201112/Week01/Sorting.java
+++ 201112/Week01/Sorting.java
@@ -9,12 +9,12 @@
 
 class Insertion_Sort {
 
-    public static void insertion_sort(long[] A) {
-	long start = System.nanoTime();
-	for (int j = 1; j < A.length; j++) {
-	    long key = A[j];
+    public static void insertion_sort(final long[] A) {
+	final long start = System.nanoTime();
+	for (int j = 1; j < A.length; ++j) {
+	    final long key = A[j];
 	    int i = j;
-	    while ((i > 0) && (A[i-1] > key)) {
+	    while (i > 0 && A[i-1] > key) {
 		A[i] = A[i-1];
 		i = i-1;
 	    }
