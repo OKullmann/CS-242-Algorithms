266d113acb819b7b2e974fd7d694df488d7e9010
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-10-17 21:10:07 +0100

    Editorial updates.

--- 201112/Week03/Recurrence.java
+++ 201112/Week03/Recurrence.java
@@ -1,24 +1,20 @@
 // Arnold Beckmann, 05.02.2009 (Swansea)
-
+// Modified Oliver Kullmann, 17.10.2011 (Swansea)
 
 /*
-  File:  201011/Week03/Recurrence_0.java
-  Descr: Experience the Master Theorem
-         Computing recurrence T_1
+  File:  201112/Week03/Recurrence.java
+  Descr: Experience the Master Theorem (recurrence T_1)
 */
-
 
 class Recurrence {
 
-    public static void recurrence(final int n) {
-	System.out.println(" " + n + " " + T(n));
-    }
+  public static void recurrence(final int n) {
+    System.out.println(" " + n + " " + T(n));
+  }
 
-    private static long T(final int n) {
-	if (n<2)
-	    return 1;
-	else
-	    return 4*T(n/2)+n;
-    }
+  private static long T(final int n) {
+    if (n<2) return 1;
+    else return 4*T(n/2)+n;
+  }
 
 }
--- 201112/Week03/solutions/Recurrence_2.java
+++ 201112/Week03/solutions/Recurrence_2.java
@@ -1,24 +1,21 @@
 // Arnold Beckmann, 09.02.2009 (Swansea)
-
+// Modified Oliver Kullmann, 17.10.2011 (Swansea)
 
 /*
-  File:  201011/Week03/solutions/Recurrence_2.java
+  File:  201112/Week03/solutions/Recurrence_2.java
   Descr: Experience the Master Theorem
          Computing recurrence T_3
 */
 
-
 class Recurrence {
 
-    public static void recurrence(final int n) {
-	System.out.println(" " + n + " " + T(n));
-    }
+  public static void recurrence(final int n) {
+    System.out.println(" " + n + " " + T(n));
+  }
 
-    private static long T(final int n) {
-	if (n<2)
-	    return 1;
-	else
-	    return 4*T(n/2)+n*n*n;
-    }
+  private static long T(final int n) {
+    if (n<2) return 1;
+    else return 4*T(n/2)+n*n*n;
+  }
 
 }
