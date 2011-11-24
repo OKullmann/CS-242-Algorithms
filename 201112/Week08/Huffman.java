9c446cf4adbbb3a862a3aac413883053de1ef77a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-11-24 02:19:34 +0000

    Editorial updates.

--- 201112/Week08/Huffman.java
+++ 201112/Week08/Huffman.java
@@ -1,106 +1,102 @@
 // Arnold Beckmann, 09.03.2010 (Swansea)
-
+// Oliver Kullmann, 24.11.2011 (Swansea)
 
 /*
-  File:  200910/Week07/Huffman.java
+  File:  201112/Week08/Huffman.java
   Descr: Huffman codes
 */
 
-
 import java.util.Hashtable;
 
-
 /*
-  The next class defines a basic compound data structure suitable
+  Class Node defines a basic compound data structure suitable
   for implementing Huffman codes.
 */
 
-
 class Node {
 
-    char c;      // the symbol to be stored
-    int f;       // the frequency
-    String h;    // Huffman code;
-    Node l;      // left  child in Huffman code (for "0")
-    Node r;      // right child in Huffman code (for "1")
-    Node (char c, int f, Node l, Node r)
-	{ this.c = c; this.f = f; this.h = ""; this.l = l; this.r =r; }
-    Node (int f, Node l, Node r)
-	{ this.c = ' '; this.f = f; this.h = ""; this.l = l; this.r =r; }
-    boolean larger(Node i) {
-	return this.f > i.f;
-    }
+    final char c;
+    final int f;   // frequency
+    final Node l;  // left  child in Huffman code (for "0")
+    final Node r;  // right child in Huffman code (for "1")
+    String h;      // resulting Huffman encoding (a binary string);
 
+    Node (final char c_, final int f_, final Node l_, final Node r_) {
+      assert(f_ >= 0);
+      c = c_; f = f_; l = l_; r =r_;
+      h = "";
+    }
+    Node (final int f_, final Node l_, final Node r_) {
+      assert(f_ >= 0);
+      c = ' '; f = f_; l = l_; r =r_;
+      h = "";
+    }
+    boolean larger(final Node i) { return this.f > i.f; }
 }
 
 
 /*
-  We now give an implementation of a min priority queue for Nodes 
+  We now give an implementation of a min priority queue for Nodes
   keyed on frequencies Node.f
  */
 class PQ {
 
-    private boolean larger(int i, int j)
-	{ return pq[i].larger(pq[j]); }
-    private void exch(int i, int j)
-	{ Node t = pq[i]; pq[i] = pq[j]; pq[j] = t; }
+    private boolean larger(final int i, final int j) {
+      return pq[i].larger(pq[j]);
+    }
+    private void exch(final int i, final int j) {
+      final Node t = pq[i]; pq[i] = pq[j]; pq[j] = t;
+    }
 
     private void swim(int k) {
-	while (k > 1 && larger(k/2, k)) 
-	    { exch(k,k/2); k = k/2; }
+	while (k > 1 && larger(k/2, k)) {
+	  exch(k,k/2); k = k/2;
+      }
     }
-    private void sink(int k, int N) {
+    private void sink(int k, final int N) {
 	while (2*k <= N) {
 	    int j = 2*k;
-	    if (j < N && larger(j,j+1)) j++;
+	    if (j < N && larger(j,j+1)) ++j;
 	    if (!larger(k,j)) break;
 	    exch(k,j); k = j;
 	}
     }
 
-    private Node[] pq;
+    private final Node[] pq;
     private int N;
 
-    PQ(int maxN)
-	{ pq = new Node[maxN+1]; N=0; }
-    int length()
-	{ return N; }
-    void insert(Node v)
-	{ pq[++N] = v; swim(N); }
-    Node getmin()
-	{ exch(1,N); sink(1, N-1); return pq[N--]; }
+    PQ(final int maxN) { pq = new Node[maxN+1]; N=0; }
+    int length() { return N; }
+    void insert(final Node v) { pq[++N] = v; swim(N); }
+    Node getmin() { exch(1,N); sink(1, N-1); return pq[N--]; }
 }
 
 
 /*
-  "Huffman" implements methods to compute and make use of 
+  "Huffman" implements methods to compute and make use of
   Huffman codes.
  */
 
-class Huffman { 
+class Huffman {
 
-    static private Hashtable M = new Hashtable();
+    static private final Hashtable M = new Hashtable();
 
 // "generate" implements the greedy algorithm for computing Huffman codes
 // from the lecture
-    public static Node generate(Node[] C) {
-	Node x,y,z;
-	int f;
-	int n = C.length;
+    public static Node generate(final Node[] C) {
+	final int n = C.length;
 
 	PQ Q = new PQ(n);
-	for (int i = 0; i < n; i++) {
-	    Q.insert(C[i]);
-	}
+	for (int i = 0; i < n; ++i) Q.insert(C[i]);
 
-	for (int i = 1; i < n; i++) {
-	    x = Q.getmin();
-	    y = Q.getmin();
-	    f = x.f + y.f;
-	    z = new Node(f,x,y);
-	    Q.insert(z);
+	for (int i = 1; i < n; ++i) {
+	  final Node x = Q.getmin();
+	  final Node y = Q.getmin();
+	  final int f = x.f + y.f;
+	  final Node z = new Node(f,x,y);
+	  Q.insert(z);
 	}
-	z = Q.getmin();
+	final Node z = Q.getmin();
 	hc("",z);
 	return z;
     }
@@ -111,10 +107,9 @@ class Huffman {
    and also in the hashtable M keyed on the char stored in the leafes.
 */
 
-    public static void hc(Node H) {
-	hc("",H);
-    }
-    private static void hc(String s, Node H) {
+    public static void hc(final Node H) { hc("",H); }
+    private static void hc(final String s, final Node H) {
+      assert(H != null);
 	boolean leaf = true;
 	if (H.l != null) { leaf = false; hc(s+"0",H.l); }
 	if (H.r != null) { leaf = false; hc(s+"1",H.r); }
@@ -123,13 +118,14 @@ class Huffman {
 
 
 // "out" outputs the stored Huffman codes
-    public static void out(Node[] C) {
+    public static void out(final Node[] C) {
+      assert(C != null);
 	int l = 0;
 	System.out.println("\n\nHuffman code\n");
-	for (int i = 0; i < C.length; i++) {
-	    System.out.printf("  char: %c   freq: %2d   code: %8s \n",
-			      C[i].c, C[i].f, C[i].h);
-	    l += C[i].f * C[i].h.length();
+	for (int i = 0; i < C.length; ++i) {
+	  System.out.printf("  char: %c   freq: %2d   code: %8s \n",
+                          C[i].c, C[i].f, C[i].h);
+	  l += C[i].f * C[i].h.length();
 	}
 	System.out.println("\nOverall length of code:  " + l + "\n");
     }
@@ -137,39 +133,35 @@ class Huffman {
 
 // "encode" encodes a string based on the Huffman code stored in the
 // hashtable M
-    public static String encode(String s) {
+    public static String encode(final String s) {
 	String o = "";
-	for (int i = 0; i < s.length(); i++) {
-	    char k = s.charAt(i);
-	    o = o + ((Node)M.get(k)).h;
+	for (int i = 0; i < s.length(); ++i) {
+	  char k = s.charAt(i);
+	  o += ((Node)M.get(k)).h;
 	}
 	return o;
     }
 
 // "decode" decodes a binary string based on the Huffman code stored
 // in the given Node structure
-    public static String decode(Node H, String s) {
-	String o = "";
-	Node w = H;
-	for (int i = 0; i < s.length(); i++) {
-	    if (s.charAt(i) == '0') {
-		if (w.l == null) {
-		    o = o + w.c;
-		    w = H.l;
-		} else
-		    w = w.l;
-	    } else {
-		if (w.r == null) {
-		    o  = o + w.c;
-		    w = H.r;
-		} else
-		    w = w.r;
-	    }
-	}
-	o = o + w.c;
-	return o;
+    public static String decode(final Node H, final String s) {
+      String o = "";
+      Node w = H;
+      for (int i = 0; i < s.length(); i++) {
+        if (s.charAt(i) == '0') {
+          if (w.l == null) {
+            o += w.c;
+            w = H.l;
+          } else w = w.l;
+	  }
+        else {
+	    if (w.r == null) {
+	      o += w.c;
+	      w = H.r;
+	    } else w = w.r;
+	  }
+      }
+      o += w.c;
+      return o;
     }
-
-}		
-
-
+}
