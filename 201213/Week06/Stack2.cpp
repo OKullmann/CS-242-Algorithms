f18d6141fed744b8f6c59aa451034012a6d9c966
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2012-11-04 15:53:07 +0000

    C++ implementation (via vector).

---
+++ 201213/Week06/Stack2.cpp
@@ -0,0 +1,31 @@
+// Oliver Kullmann, 4.11.2012 (Swansea)
+
+#include <cassert>
+#include <iostream>
+
+#include "Stack.hpp"
+
+int main(const int argc, const char* const argv) {
+  typedef Buffers::Stack<int> Stack;
+  Stack S0;
+  assert(S0.empty());
+  assert(S0.size() == 0);
+
+  Stack S1;
+  S1.push(33).push(44).push(55);
+  std::cout << "S1:" << S1;
+
+  Stack S2(S1);
+  assert(S1 == S2);
+  std::cout << S2.top() << "\n";
+  S2.pop().pop();
+  std::cout << "S2:" << S2;
+
+  S2 = S0;
+  std::cout << "S2:" << S2;
+
+  S0 = S1;
+  std::cout << "S0:" << S0 << "\n";
+  S0.reserve(1000);
+  std::cout << "S0:" << S0 << "\n";
+}
