2e6c46ddbd70bc293b61a3713dd383511e32ac2a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2012-11-11 16:26:19 +0000

    Some improvements.

--- 201213/Week07/BinarySearch.java
+++ 201213/Week07/BinarySearch.java
@@ -2,7 +2,8 @@
 
 class BinarySearch {
 
-  // Searches for x in sorted A, from begin (included) to end (excluded), and returns some
+  // Searches for x in the range of array A from begin (included) to end (excluded),
+  // where this range needs to be sorted for the search to be complete, and returns some
   // such index, if it exists, and -1 otherwise:
   public static int binary_search(final int[] A, int begin, int end, final int x) {
     if (A == null) return -1;
@@ -27,6 +28,10 @@ class BinarySearch {
       else end = mid;
     }
   }
+  public static int binary_search(final int[] A, final int x) {
+    if (A == null) return -1;
+    return binary_search(A, 0, A.length, x);
+  }
 
   public static void main(final String[] args) {
     if (args.length == 0) {
@@ -37,7 +42,7 @@ class BinarySearch {
     final int x = Integer.parseInt(args[N]);
     final int[] A = new int[N];
     for (int i = 0; i != N; ++i) A[i] = Integer.parseInt(args[i]);
-    System.out.println( binary_search(A, 0, N, x));
+    System.out.println(binary_search(A, x));
   }
 
 }
