94a57888d88f7efebc62b9f64387794128039356
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2015-12-16 17:22:40 +0000

    Indeed, j is never begin in the final version.
    And some other editorial improvements.

--- AlgorithmsModernC++/Sorting/InsertionSort.hpp
+++ AlgorithmsModernC++/Sorting/InsertionSort.hpp
@@ -1,15 +1,16 @@
 // Oliver Kullmann, 3.12.2015 (Swansea)
 
 /* Various implementations of insertion sort
+
    The basic idea of insertion sort of a vector v of length n is:
-      incrementally sort v[1],...,v[i] for i=1,...,n.
-   Sorting of v[1],...,v[i] exploits that v[1],...,v[i-1] is already
+      incrementally sort v[0],...,v[i] for i=1,...,n-1.
+   Sorting of v[0],...,v[i] exploits that v[0],...,v[i-1] is already
    sorted, and just needs to *insert* v[i].
 
-   Version 0 below "inserts" via consecutive swaps, version 1 by shifting.
+   Version 0 below "inserts" via consecutive swaps, Version 1 by shifting.
    Version 2 (apparently OK's "invention") optimises this, by saving
-   on the auxiliary actions, and the final version (apparently fastest)
-   removes random access, and only exploits bidirectional iterators.
+   on the auxiliary actions, and the final version removes random access, and
+   only exploits bidirectional iterators.
 */
 
 #ifndef INSERTIONSORT_WEUhALkpF8
@@ -35,10 +36,9 @@ namespace Sort {
   inline void insertion1(V& v) {
     if (v.empty()) return;
     typedef typename V::size_type size_t;
-    typedef typename V::value_type val_t;
     const size_t size = v.size();
     for (size_t i = 1; i != size; ++i) {
-      const val_t x = v[i];
+      const auto x = v[i];
       size_t j;
       for (j = i; j != 0 and v[j-1] > x; --j) v[j] = v[j-1];
       v[j] = x;
@@ -92,11 +92,11 @@ namespace Sort {
   // iterators:
   template <class It>
   inline void insertion(const It begin, const It end) {
-    typedef typename It::value_type val_t;
     if (begin == end) return;
     It i = begin;
     if (++i == end) return;
     if (*begin > *i) std::swap(*i, *begin);
+    typedef typename It::value_type val_t;
     val_t y = *i;
     if (++i == end) return;
     do {
@@ -104,12 +104,10 @@ namespace Sort {
       if (y > x) {
         *i = y;
         It j = i; --j;
-        if (j != begin) {
-          It j2 = j; --j2;
-          for (val_t z; (z = *j2) > x; --j2) {
-            *j = z;
-            if ((j = j2) == begin) break;
-          }
+        It j2 = j; --j2;
+        for (val_t z; (z = *j2) > x; --j2) {
+          *j = z;
+          if ((j = j2) == begin) break;
         }
         *j = x;
       }
