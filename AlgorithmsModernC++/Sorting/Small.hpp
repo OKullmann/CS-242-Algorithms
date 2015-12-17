8b4e2fae0b73c52e424e70901f67a75d1029aebd
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2015-12-17 01:51:08 +0000

    Improved comments.

--- AlgorithmsModernC++/Sorting/Small.hpp
+++ AlgorithmsModernC++/Sorting/Small.hpp
@@ -2,6 +2,12 @@
 
 /* Inplace sorting of small fixed-size arrays
 
+   As the array [1,0,0] shows, when stability is required, then more
+   assignments are needed; thus the algorithms below for >= 3 elements
+   are not stable.
+
+   TODO: Can size3 be improved? It seems the logic could be streamlined.
+
 */
 
 #ifndef SMALL_fCiBIsrRrm
@@ -18,22 +24,23 @@ namespace Sort {
 
   template <class V>
   inline void size3_(V& v) {
-    if (v[0] > v[1]) // 1,0,x
-      if (v[2] > v[0]) // 1,0,2
+    if (v[0] > v[1]) // 1<0,x
+      if (v[2] > v[0]) // 1<0<2
         std::swap(v[0], v[1]); 
-      else // 1,x,0
-        if (v[2] > v[1]) // 1,2,0
+      else // 1,x<=0
+        if (v[2] > v[1]) // 1<2<=0
           {const auto t=v[0]; v[0]=v[1]; v[1]=v[2]; v[2]=t;}
-        else // 2,1,0
+        else // 2<=1<0
           std::swap(v[0], v[2]);
-    else // 0,1,x
-      if (v[1] > v[2]) // 0,x,1
-        if (v[0] > v[2]) // 2,0,1
+    else // 0<=1,x
+      if (v[1] > v[2]) // 0,x<1
+        if (v[0] > v[2]) // 2<0<=1
           {const auto t=v[0]; v[0]=v[2]; v[2]=v[1]; v[1]=t;}
-        else // 0,2,1
+        else // 0<=2<=1
           std::swap(v[1], v[2]);
   }
 
+  // Same as size3_, but now just using forward iterators:
   template <class It>
   inline void size3(const It begin, const It end) {
     const It a = begin;
