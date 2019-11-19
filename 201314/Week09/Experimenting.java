10eac8d1eaef74a659980fe7dcb2ddc305ecde29
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2019-11-19 14:56:54 +0000

    Code for experiment according to labsheet.

--- 201314/Week09/Experimenting.java
+++ 201314/Week09/Experimenting.java
@@ -2,5 +2,7 @@ class Experimenting {
   public static void main(final String[] args) {
     final int m = Integer.parseInt(args[0]);
     final Chaining C = new Chaining(m);
-    // XXX
+    for (int i = 1; i <= 100; ++i) C.insert(i);
+    System.out.println(C);
+  }
 }
