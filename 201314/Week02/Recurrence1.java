a4afc9f6ad0a7744b87fd167efa42ce1bff1d449
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2020-10-21 16:40:44 +0100

    Simple recurrence (first new question).

---
+++ 201314/Week02/Recurrence1.java
@@ -0,0 +1,18 @@
+class Recurrence1 {
+  public static long rec(final long n) {
+    assert(n >= 1);
+    if (n == 1) return 1;
+    if (n % 2 == 0) return 3*rec(n/2) + 2 + n;
+    return rec(n/2+1) + 2*rec(n/2) + 2 + n;
+  }
+
+  public static final long default_n = 410000000000L;
+    // rec1(default_n) == 9100131083681673202
+
+  public static void main(final String[] args) {
+    final long n;
+    if (args.length == 0) n = default_n;
+    else n = Long.parseLong(args[0]);
+    System.out.println(n + " " + rec(n));
+  }
+}
