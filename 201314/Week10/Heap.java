9dc38eccf21c818d681f8c522e46f599d42ac6d3
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2020-12-03 16:44:05 +0000

    Update, and simplifications.
    Now just processing the command-line.

--- 201314/Week10/Heap.java
+++ 201314/Week10/Heap.java
@@ -1,11 +1,5 @@
-// Oliver Kullmann, 6.12.2012 (Swansea)
-
-// Max-Heap operations
-// Run with
-// > java -ea Heap
-// to enable the assertions.
-
-import java.util.Arrays; // for clone(), equals() and sort()
+// For the example from the lecture:
+// java -ea Heap 4 1 3 2 16 9 10 14 8
 
 class Heap {
   private final int[] H;
@@ -18,15 +12,16 @@ class Heap {
   }
   
   public int size() { return n; }
-  public boolean set_size(final int n_) {
-    if (n_ < 0 || n_ > H.length) return false;
-    n = n_;
-    return true;
-  }
   
   // children, left and right:
-  private int ls(final int x) { return 2*x+1; }
-  private int rs(final int x) { return 2*x+2; }
+  private int ls(final int x) {
+    if (! leaf(x)) return 2*x+1;
+    else return n;
+  }
+  private int rs(final int x) {
+    if (! leaf(x)) return 2*x+2;
+    else return n;
+  }
   // parent:
   private int p(final int x) { return (x-1)/2; }
   
@@ -35,13 +30,8 @@ class Heap {
   private boolean leaf(final int i) { return i >= first_leaf(); }
   
   public boolean heap_property() {
-    final int fl = first_leaf();
-    for (int i = 0; i < fl; ++i) {
-      final int left_child = ls(i);
-      final int right_child = rs(i);
-      if (H[i] < H[left_child]) return false;
-      if (right_child < n && H[i] < H[right_child]) return false;
-    }
+    for (int i = 1; i < n; ++i)
+      if (H[p(i)] < H[i]) return false;
     return true;
   }
   
@@ -61,13 +51,15 @@ class Heap {
   public void build_heap() {
     final int fl = first_leaf();
     for (int i = fl-1; i >= 0; --i) heapify(i);
+    assert(heap_property());
   }
   
-  public void sort() {
-    build_heap();
+  public void sort(final boolean log) {
+    if (log) print();
+    build_heap(); if (log) print();
     while (n > 0) {
       {final int t = H[0]; H[0] = H[--n]; H[n] = t;}
-      heapify(0);
+      heapify(0); if (log) print();
     }
   }
   
@@ -76,30 +68,18 @@ class Heap {
     for (int i = 0; i < H.length; ++i) res += " " + H[i];
     return res;
   }
+  private int counter = 0;
+  private void print() {
+    System.out.printf("%-5d  ", counter++);
+    System.out.println(this);
+  }
   
   public static void main(final String[] args) {
-    {
-     final int[] a = {0,1};
-     final Heap H = new Heap(a);
-     assert(! H.heap_property());
-    }
-    {
-     final int[] a = {16,14,10,8,7,9,3,2,4,1};
-     final Heap H = new Heap(a);
-     assert(H.heap_property());
-     final int[] b = {4,1,3,2,16,9,10,14,8,7};
-     final int[] c = b.clone();
-     final Heap H2 = new Heap(b);
-     assert(! H2.heap_property());
-     H2.build_heap();
-     assert(Arrays.equals(a,b));
-     final Heap H3 = new Heap(c);
-     H3.sort();
-     Arrays.sort(a);
-     assert(Arrays.equals(a,c));
-    }
-  }
-  {
-    // here comes your code, for Part 2
+    final int[] a = new int[args.length];
+    for (int i = 0; i < args.length; ++i)
+      a[i] = Integer.parseInt(args[i]);
+    Heap H = new Heap(a);
+    H.sort(true);
+    return;
   }
 }
