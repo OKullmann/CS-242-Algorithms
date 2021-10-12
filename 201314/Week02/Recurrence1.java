d65ee15e2fac4e8aea55c0fd12a79b47b882182f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2021-10-13 00:23:51 +0100

    Printing now also quotients.

--- 201314/Week02/Recurrence1.java
+++ 201314/Week02/Recurrence1.java
@@ -1,18 +1,35 @@
 class Recurrence1 {
+
+  public static final long max_n = 411175444467L;
+
   public static long rec(final long n) {
     assert(n >= 1);
+    assert(n <= max_n);
     if (n == 1) return 1;
-    if (n % 2 == 0) return 3*rec(n/2) + 2 + n;
-    return rec(n/2+1) + 2*rec(n/2) + 2 + n;
+    else if (n % 2 == 0) return 3*rec(n/2) + 2 + n;
+    else return rec(n/2+1) + 2*rec(n/2) + 2 + n;
   }
 
-  public static final long default_n = 410000000000L;
-    // rec(default_n) == 9100131083681673202
-
   public static void main(final String[] args) {
-    final long n;
-    if (args.length == 0) n = default_n;
-    else n = Long.parseLong(args[0]);
-    System.out.println(n + " " + rec(n));
+    assert(rec(1) == 1);
+    assert(rec(2) == 7);
+    assert(rec(410000000000L) == 9100131083681673202L);
+    assert(rec(max_n) == 9223372031224988194L);
+
+    final int N = args.length;
+    if (N == 0) return;
+    final long a[] = new long[N];
+    for (int i = 0; i < N; ++i)
+      a[i] = Long.parseLong(args[i]);
+
+    final long res0 = rec(a[0]);
+    System.out.println(a[0] + " " + res0);
+    for (int i = 1; i < N; ++i) {
+      final long res = rec(a[i]);
+      System.out.print(a[i] + " " + res);
+      final double q1 = (double) a[i] / a[0];
+      final double q2 = (double) res / res0;
+      System.out.println(" " + q1 + " " + q2);
+    }
   }
 }
