7f566234e4bd181e9a12d78c56999c40a026ed68
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2021-10-13 07:46:23 +0100

    Similar update as with Recurrence1.java.

--- 201314/Week02/Recurrence2.java
+++ 201314/Week02/Recurrence2.java
@@ -1,18 +1,33 @@
 class Recurrence2 {
+
+  public static final long max_n = 150000000000000000L;
+
   public static long rec(final long n) {
     assert(n >= 1);
+    assert(n <= max_n);
     if (n == 1) return 1;
-    if (n % 2 == 0) return 2*rec(n/2) + n;
-    return rec(n/2+1) + rec(n/2) + n;
+    else if (n % 2 == 0) return 2*rec(n/2) + n;
+    else return rec(n/2+1) + rec(n/2) + n;
   }
 
-  public static final long default_n = 150000000000000000L;
-    // rec(default_n) == 8711769623848288256
-
   public static void main(final String[] args) {
-    final long n;
-    if (args.length == 0) n = default_n;
-    else n = Long.parseLong(args[0]);
-    System.out.println(n + " " + rec(n));
+    assert(rec(1) == 1);
+    assert(rec(max_n) == 8711769623848288256L);
+
+    final int N = args.length;
+    if (N == 0) return;
+    final long a[] = new long[N];
+    for (int i = 0; i < N; ++i)
+      a[i] = Long.parseLong(args[i]);
+
+    final long res0 = rec(a[0]);
+    System.out.println(a[0] + " " + res0);
+    for (int i = 1; i < N; ++i) {
+      final long res = rec(a[i]);
+      System.out.print(a[i] + " " + res);
+      final double q1 = (double) a[i] / a[0];
+      final double q2 = (double) res / res0;
+      System.out.println(" " + q1 + " " + q2);
+    }
   }
 }
