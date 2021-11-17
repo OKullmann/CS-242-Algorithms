c8ec8b5f1f883448959bc249c7217a2810da805d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2021-11-17 09:01:18 +0000

    "Experiment" nun in Chaining enthalten.

--- 201314/Week09/Chaining.java
+++ 201314/Week09/Chaining.java
@@ -37,25 +37,38 @@ class Chaining {
   }
   
   public static void main(final String[] args) {
+    if (args.length == 0) {
+      System.err.println("[CHAINING]: m [N] [step]");
+      System.exit(1);
+    }
     final int m = Integer.parseInt(args[0]);
     final Chaining C = new Chaining(m);
 
-    C.insert(5); C.insert(28); C.insert(19); C.insert(15);
-    C.insert(20); C.insert(33); C.insert(12); C.insert(17);
-    C.insert(10);
-    System.out.println(C);
-    System.out.println(C.search(1));
-    System.out.println("****\n");
+    if (args.length == 1) {
+      C.insert(5); C.insert(28); C.insert(19); C.insert(15);
+      C.insert(20); C.insert(33); C.insert(12); C.insert(17);
+      C.insert(10);
+      System.out.println(C);
+      System.out.println(C.search(1));
+      System.out.println("****\n");
 
-    final List f1 = C.search(19);
-    C.delete(f1);
-    System.out.println(C);
-    System.out.println("****\n");
+      final List f1 = C.search(19);
+      C.delete(f1);
+      System.out.println(C);
+      System.out.println("****\n");
 
-    C.insert(5);
-    final List f2 = C.search(12);
-    C.delete(f2);
-    System.out.println(C);
-    System.out.println("****");
+      C.insert(5);
+      final List f2 = C.search(12);
+      C.delete(f2);
+      System.out.println(C);
+      System.out.println("****");
+    }
+    else {
+      final int N = Integer.parseInt(args[1]);
+      final int step =
+        args.length == 3 ? Integer.parseInt(args[2]) : 1;
+      for (int i = 0; i < N; i+=step) C.insert(i);
+      System.out.println(C);
+    }
   }
 }
