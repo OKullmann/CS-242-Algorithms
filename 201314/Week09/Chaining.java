3f359c6c1b7972969933d18c1e5758bb4ad19f7c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2021-11-17 08:37:50 +0000

    Improved output.

--- 201314/Week09/Chaining.java
+++ 201314/Week09/Chaining.java
@@ -32,39 +32,30 @@ class Chaining {
     String res = "";
     for (int i = 0; i < m; ++i)
       res += i + ": " + List.show(T[i]) + "\n";
+    res += "size=" + size() + "\n";
     return res;
   }
   
-  /*
-  public boolean equals(final Chaining C) {
-    if (s != C.size()) return false;
-    for (int i = 0; i < m; ++i)
-      for (List p = T[i]; p != null; p = p.next)
-        if (C.search(p.key) == null) return false;
-    for (int i = 0; i < C.m; ++i)
-      for (List p = C.T[i]; p != null; p = p.next)
-        if (search(p.key) == null) return false;
-    return true;
-  }
-  */
-
   public static void main(final String[] args) {
     final int m = Integer.parseInt(args[0]);
     final Chaining C = new Chaining(m);
+
     C.insert(5); C.insert(28); C.insert(19); C.insert(15);
     C.insert(20); C.insert(33); C.insert(12); C.insert(17);
     C.insert(10);
     System.out.println(C);
-    System.out.println(C.size());
     System.out.println(C.search(1));
+    System.out.println("****\n");
+
     final List f1 = C.search(19);
     C.delete(f1);
     System.out.println(C);
-    System.out.println(C.size());
+    System.out.println("****\n");
+
     C.insert(5);
     final List f2 = C.search(12);
     C.delete(f2);
     System.out.println(C);
-    System.out.println(C.size());
+    System.out.println("****");
   }
 }
