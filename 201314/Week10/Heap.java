cc27a2c6c153617ed053b9b71ee24d8e4533c248
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2021-11-24 11:07:45 +0000

    Verschiedene kleinere Verbesserungen.

--- 201314/Week10/Heap.java
+++ 201314/Week10/Heap.java
@@ -13,21 +13,33 @@ class Heap {
 
   public int size() { return n; }
 
-  // children, left and right:
-  private int ls(final int x) {
-    if (! leaf(x)) return 2*x+1;
-    else return n;
+  private boolean valid(final int x) {
+    return x >= 0 && x < n;
   }
-  private int rs(final int x) {
-    if (! leaf(x)) return 2*x+2;
-    else return n;
-  }
-  // parent:
-  private int p(final int x) { return (x-1)/2; }
 
   // the first index of a leaf:
   private int first_leaf() { return n/2; }
-  private boolean leaf(final int i) { return i >= first_leaf(); }
+  // whether i is a leaf:
+  private boolean leaf(final int i) {
+    assert(valid(i));
+    return i >= first_leaf();
+  }
+
+  // children, left and right:
+  private int ls(final int x) {
+    assert(! leaf(x));
+    return 2*x+1;
+  }
+  // returning n in case there is no right child:
+  private int rs(final int x) {
+    assert(! leaf(x));
+    return Math.min(2*x+2, n);
+  }
+  // parent (returning 0 for the root):
+  private int p(final int x) {
+    assert(valid(x));
+    return (x-1)/2;
+  }
 
   public boolean heap_property() {
     for (int i = 1; i < n; ++i)
@@ -35,13 +47,17 @@ class Heap {
     return true;
   }
 
+  // floating down a single disturbance at node i:
   private void heapify(final int i) {
+    assert(valid(i));
     if (leaf(i)) return;
     final int v = H[i];
     final int left_child = ls(i);
     final int right_child = rs(i);
     int largest = (v >= H[left_child]) ? i : left_child;
-    if (right_child < n && H[largest] < H[right_child]) largest = right_child;
+    if (right_child != n && H[largest] < H[right_child])
+      largest = right_child;
+    assert(valid(largest));
     if (largest != i) {
       H[i] = H[largest]; H[largest] = v;
       heapify(largest);
@@ -56,13 +72,18 @@ class Heap {
 
   public void sort(final boolean log) {
     if (log) print();
+    if (n <= 1) return;
     build_heap(); if (log) print();
-    while (n > 0) {
+    while (true) {
       {final int t = H[0]; H[0] = H[--n]; H[n] = t;}
+      if (n == 0) return;
       heapify(0); if (log) print();
     }
   }
 
+  // output
+  //   "(n, total length): array"
+  // with ";" indicating end of current binary heap:
   public String toString() {
     String res = String.format("(%2d,%2d):", n, H.length);
     for (int i = 0; i < n; ++i)
@@ -75,6 +96,7 @@ class Heap {
     }
     return res;
   }
+  // print with additional leading step-counter:
   private int counter = 0;
   private void print() {
     System.out.printf("%-5d  ", counter++);
@@ -85,8 +107,8 @@ class Heap {
     final int[] a = new int[args.length];
     for (int i = 0; i < args.length; ++i)
       a[i] = Integer.parseInt(args[i]);
+
     Heap H = new Heap(a);
     H.sort(true);
-    return;
   }
 }
